c8e2ab7b43ca74d6093bbb0e42fa579151357f08
Josh <richardsonjosh03@gmail.com>
2025-09-17 20:35:41 -0700

    initial commit

---
+++ calculations.js
@@ -0,0 +1,29 @@
+document.getElementById('inputForm').addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    const numberOfBooklets = document.getElementById('numBooklets').value;
+    const numberOfBookletPages = document.getElementById('bookletPages').value;
+    const bookletHeight = document.getElementById('bookletHeight').value;
+    const bookletWidth = document.getElementById('bookletWidth').value;
+    output = document.getElementById('output');
+
+    if (numberOfBookletPages == 0 || numberOfBooklets == 0 || bookletHeight == 0 || bookletWidth == 0) {
+        output.innerHTML = "";
+        const myModal = new bootstrap.Modal(document.getElementById('exampleModal'));
+        myModal.show();
+        return;
+    }
+
+    const sheetsPerBooklet = Math.ceil(numberOfBookletPages / 4);
+    const totalSheets = sheetsPerBooklet * numberOfBooklets;
+    const numBlankPages = numberOfBookletPages % 4;
+    const sheetSize = bookletWidth * 2;
+
+    output.innerHTML = `
+    ${numberOfBookletPages > 48 ? "WARNING Thick booklets may not lay flat <br>" : ""}
+    Total sheets: ${totalSheets} <br>
+    Sheets per booklet: ${sheetsPerBooklet} <br>
+    ${numBlankPages > 0 ? "Blank pages added: ${numBlankPages} <br>" : ""} 
+    Size of sheet before folding: ${sheetSize}" x ${bookletHeight}" <br>
+    `
+});
